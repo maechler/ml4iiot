47eda41c0e9652138c74ca6b536844fb08b5f8fd
Markus Mächler <markus.maechler@bithost.ch>
2019-10-18 07:18:31 +0200

    Adds support for JSON configurations

--- ml4iiot/cli_runner.py
+++ ml4iiot/cli_runner.py
@@ -1,22 +1,32 @@
 import argparse
 import yaml
+import json
 from ml4iiot.pipeline.pipeline import Pipeline
 
 
-def run(config_path):
-    config_file = open(config_path)
-    config = yaml.load(config_file, Loader=yaml.FullLoader)
+def run(config_path, format):
+    if format == 'yaml':
+        config_file = open(config_path)
+        config = yaml.load(config_file, Loader=yaml.FullLoader)
+        config_file.close()
+    elif format == 'json':
+        config_file = open(config_path)
+        config = json.load(config_file)
+        config_file.close()
+    else:
+        raise ValueError('Config format "{0}" is not supported.'.format(format))
+
     pipeline = Pipeline(config['pipeline'])
 
-    config_file.close()
     pipeline.run()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-c', '--config_path', help='Path to a yaml config file.', type=str, required=True)
+    parser.add_argument('-c', '--config_path', help='Path to a config file.', type=str, required=True)
+    parser.add_argument('-f', '--format', help='Format of the config file.', type=str, required=True, default='yaml')
 
     args = parser.parse_args()
 
-    run(args.config_path)
+    run(args.config_path, args.format)
