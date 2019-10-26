23901e381561423865226817a3e989e2bcb1358a
Markus Mächler <markus.maechler@bithost.ch>
2019-10-26 09:51:57 +0200

    Adds simple min max scaler

---
+++ ml4iiot/postprocessing/normalization/minmaxscaler.py
@@ -0,0 +1,5 @@
+from ml4iiot.preprocessing.normalization.minmaxscaler import MinMaxScaler as PrePorcessingMinMaxScaler
+
+
+class MinMaxScaler(PrePorcessingMinMaxScaler):
+    pass
---
+++ ml4iiot/preprocessing/normalization/minmaxscaler.py
@@ -0,0 +1,33 @@
+from sklearn.preprocessing import MinMaxScaler as ScikitLearnMinMaxScaler
+from ml4iiot.pipeline.step import AbstractStep
+
+
+class MinMaxScaler(AbstractStep):
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.scaler = ScikitLearnMinMaxScaler(feature_range=(0, 1))
+        self.column_mapping = self.get_config('column_mapping')
+        self.source_column = self.get_config('source_column', default=None)
+
+    def process(self, data_frame):
+        if self.source_column is None:
+            self.normalize(data_frame)
+        else:
+            self.denormalize(data_frame)
+
+    def normalize(self, data_frame):
+        for source_column, target_column in self.column_mapping.items():
+            source = data_frame[[source_column]]
+            normalized = self.scaler.fit_transform(source)
+            data_frame[target_column] = normalized[:, 0]
+
+    def denormalize(self, data_frame):
+        for source_column, target_column in self.column_mapping.items():
+            source = data_frame[[self.source_column]]
+            source_n = data_frame[[source_column]]
+
+            self.scaler.fit(source)
+
+            reconstruction = self.scaler.inverse_transform(source_n)
+            data_frame[target_column] = reconstruction[:, 0]
