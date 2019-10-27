ac3a5f11474f80121dcf8844f309b4c991a8715a
Markus Mächler <markus.maechler@bithost.ch>
2019-10-27 20:38:26 +0100

    Adds MAD algorithm

---
+++ ml4iiot/algorithm/stochastic/mad.py
@@ -0,0 +1,22 @@
+from pandas import DataFrame
+from ml4iiot.algorithm.abstractalgorithm import AbstractAlgorithm
+import numpy as np
+
+
+class Mad(AbstractAlgorithm):
+
+    def __init__(self, config):
+        super().__init__(config)
+
+        self.column_mapping = self.get_config('column_mapping')
+
+    def process(self, data_frame: DataFrame) -> None:
+        for source_column, target_column in self.column_mapping.items():
+            data = np.array(data_frame[source_column].values)
+            median = np.median(data)
+            deviations = data - median
+            absolute_deviations = np.abs(deviations)
+            mad = np.median(absolute_deviations)
+
+            data_frame[target_column] = float('nan')
+            data_frame[target_column].iloc[-1] = mad
