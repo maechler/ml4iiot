cb95712b71d697cd1986ac800154d5ea82eac921
Markus Mächler <markus.maechler@bithost.ch>
2019-12-14 19:29:50 +0100

    Adds columns setting to CSV output

--- ml4iiot/output/csv.py
+++ ml4iiot/output/csv.py
@@ -11,6 +11,7 @@ class CsvOutput(AbstractOutput):
         self.do_output_condition = instance_from_config(self.get_config('do_output_condition', default={'class': 'ml4iiot.conditions.TrueCondition'}))
         self.output_file_path = get_absolute_path(self.get_config('output_file_path', default='./out/csv_output.csv'))
         self.date_format = self.get_config('date_format', default='%s')
+        self.columns = self.get_config('columns', default=None)
         self.output_file = None
         self.write_header = True
 
@@ -21,7 +22,7 @@ class CsvOutput(AbstractOutput):
 
     def process(self, data_frame: DataFrame) -> None:
         if self.do_output_condition.evaluate(data_frame):
-            data_frame.to_csv(self.output_file, header=self.write_header, date_format=self.date_format)
+            data_frame.to_csv(self.output_file, header=self.write_header, date_format=self.date_format, columns=self.columns)
 
             self.write_header = False
 
